af8858bdcd5933245c5fb14b4f0600dd225eee43
Godfred Bekoe Ramzy <c5b2s557c8@privaterelay.appleid.com>
2026-06-11 10:32:21 +0000

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("X          XXXXXXXX");
+        System.out.println("X     X           X");
+        System.out.println("X     X           X");
+        System.out.println("XXXXX X     XXXXXXX");
+        System.out.println("      X     X     X");
+        System.out.println("      X     X     X");
+    }
+}
